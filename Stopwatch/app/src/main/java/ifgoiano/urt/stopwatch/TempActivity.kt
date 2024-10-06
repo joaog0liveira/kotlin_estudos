02c15b34f7ed085be4862b195d5f86ae0862212b
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2024-10-06 19:49:55 -0300

    06/10

--- Stopwatch/app/src/main/java/ifgoiano/urt/stopwatch/TempActivity.kt
+++ Stopwatch/app/src/main/java/ifgoiano/urt/stopwatch/TempActivity.kt
@@ -1,26 +1,85 @@
 package ifgoiano.urt.stopwatch
 
+import android.R
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ifgoiano.urt.stopwatch.databinding.ActivityTempBinding
 
 class TempActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTempBinding
+    private var countdownTimer: CountDownTimer? = null
+    private var tempoSobrando: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityTempBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val timeOptions = arrayOf("10 segundos", "30 segundos", "1 minuto", "5 minutos", "10 minutos", "30 minutos", "1 hora")
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, timeOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.timeSpinner.adapter = adapter
+
+        binding.startButton.setOnClickListener {
+            val selectedTime = binding.timeSpinner.selectedItem.toString()
+            tempoSobrando = when (selectedTime) {
+                "10 segundos" -> 10 * 1000
+                "30 segundos" -> 30 * 1000
+                "1 minuto" -> 1 * 60 * 1000
+                "5 minutos" -> 5 * 60 * 1000
+                "10 minutos" -> 10 * 60 * 1000
+                "30 minutos" -> 30 * 60 * 1000
+                "1 hora" -> 1 * 60 * 60 * 1000
+                else -> 0
+            }
+            startTimer()
         }
+
+        binding.resetButton.setOnClickListener {
+            resetTimer()
+        }
+
+        updateTimerText()
+    }
+
+    private fun startTimer() {
+        countdownTimer?.cancel()
+
+        countdownTimer = object : CountDownTimer(tempoSobrando, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                tempoSobrando = millisUntilFinished
+                updateTimerText()
+            }
+
+            override fun onFinish() {
+                binding.timerTextView.text = "Tempo esgotado!"
+            }
+        }.start()
+    }
+
+    private fun resetTimer() {
+        countdownTimer?.cancel()
+        tempoSobrando = 0
+        updateTimerText()
+    }
+
+    private fun updateTimerText() {
+        val minutes = (tempoSobrando / 1000) / 60
+        val seconds = (tempoSobrando / 1000) % 60
+
+        val timeFormatted = String.format("%02d:%02d", minutes, seconds)
+        binding.timerTextView.text = timeFormatted
+    }
+
+    override fun onPause() {
+        super.onPause()
+        countdownTimer?.cancel()
     }
 }
+
 
