6e7ac0fabc5de0d8abad009375b1200035141e45
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2024-10-11 13:28:16 -0300

    11/10

--- forms/app/src/main/java/ifgoiano/urt/forms/MainActivity.kt
+++ forms/app/src/main/java/ifgoiano/urt/forms/MainActivity.kt
@@ -1,14 +1,12 @@
 package ifgoiano.urt.forms
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ifgoiano.urt.forms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,14 +20,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        // carregar os Estados
         val estadosDisponiveis = resources.getStringArray(R.array.estados)
 
-        // criando adapter para o spinner
         val adapter = ArrayAdapter(
             this,
             android.R.layout.simple_spinner_item,
-            estadosDisponiveis
+            estadosDisponiveis,
+
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerEstados.adapter = adapter
@@ -63,6 +60,53 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerEstados.adapter = adapter
+
+        binding.spinnerEstados.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+
+                    val estadoSelecionado = parent.getItemAtPosition(position).toString()
+
+                    val prefeitosEstados = when (estadoSelecionado) {
+                        "Goiás" -> R.array.candidatos_goias
+                        "Rio de Janeiro" -> R.array.candidatos_rio
+                        "São Paulo" -> R.array.candidatos_sp
+                        else -> null
+                    }
+
+                    val vereadorEstados = when (estadoSelecionado) {
+                        "Goiás" -> R.array.vereador_go
+                        "Rio de Janeiro" -> R.array.vereador_rio
+                        "São Paulo" -> R.array.vereador_sp
+                        else -> null
+                    }
+
+                    val prefeitosAdapter = prefeitosEstados?.let {
+                        ArrayAdapter.createFromResource(
+                            this@MainActivity, it, android.R.layout.simple_spinner_item
+                        )
+                    }
+
+                    val vereadoresAdapter = vereadorEstados?.let {
+                        ArrayAdapter.createFromResource(
+                            this@MainActivity, it, android.R.layout.simple_spinner_item
+                        )
+                    }
+
+                    if (prefeitosAdapter != null && vereadoresAdapter != null) {
+                        prefeitosAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        vereadoresAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    }
+                    binding.spinnerPrefeitos.adapter = prefeitosAdapter
+                    binding.spinnerVereadores.adapter = vereadoresAdapter
+                }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+
         binding.btnConcluir.setOnClickListener {
             coletarDados()
         }
@@ -77,17 +121,22 @@ class MainActivity : AppCompatActivity() {
         val estado = binding.spinnerEstados.selectedItem.toString()
         val prefeito = binding.spinnerPrefeitos.selectedItem.toString()
 
-        val partidosPreferidos = mutableListOf<String>()
-        if (binding.partido1.isChecked) partidosPreferidos.add(binding.partido1.text.toString())
-        if (binding.partido2.isChecked) partidosPreferidos.add(binding.partido2.text.toString())
-        if (binding.partido3.isChecked) partidosPreferidos.add(binding.partido3.text.toString())
+        val partidos = mutableListOf<String>()
+        if (binding.partido1.isChecked) partidos.add(binding.partido1.text.toString())
+        if (binding.partido2.isChecked) partidos.add(binding.partido2.text.toString())
+        if (binding.partido3.isChecked) partidos.add(binding.partido3.text.toString())
 
+        val intent = Intent(this, ResultadoActivity::class.java)
 
-        val mensagem = "Nome: $nome\nTítulo de Eleitor: $tituloEleitor" +
-                "\nZona: $zona\nSeção: $secao\nCidade: $cidade" +
-                "\nEstado: $estado\nPrefeito: $prefeito" +
-                "\nPartidos Preferidos: ${partidosPreferidos.joinToString(", ")}"
+        intent.putExtra("nome", nome)
+        intent.putExtra("tituloEleitor", tituloEleitor)
+        intent.putExtra("zona", zona)
+        intent.putExtra("secao", secao)
+        intent.putExtra("cidade", cidade)
+        intent.putExtra("estado", estado)
+        intent.putExtra("prefeito", prefeito)
+        intent.putStringArrayListExtra("partidos", ArrayList(partidos))
 
-        Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show()
+        startActivity(intent)
     }
 }
--- forms/app/src/main/java/ifgoiano/urt/forms/ResultadoActivity.kt
+++ forms/app/src/main/java/ifgoiano/urt/forms/ResultadoActivity.kt
@@ -3,18 +3,40 @@ package ifgoiano.urt.forms
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ifgoiano.urt.forms.databinding.ActivityResultadoBinding
 
 class ResultadoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultadoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_resultado)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityResultadoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val nome = intent.getStringExtra("nome")
+        val tituloEleitor = intent.getStringExtra("tituloEleitor")
+        val zona = intent.getStringExtra("zona")
+        val secao = intent.getStringExtra("secao")
+        val cidade = intent.getStringExtra("cidade")
+        val estado = intent.getStringExtra("estado")
+        val prefeito = intent.getStringExtra("prefeito")
+        val partidosPreferidos = intent.getStringArrayListExtra("partidos")
+
+        binding.resultado.text = """
+            Nome: $nome
+            Título de Eleitor: $tituloEleitor
+            Zona: $zona
+            Seção: $secao
+            Cidade: $cidade
+            Estado: $estado
+            Prefeito: $prefeito
+            Partidos Preferidos: ${partidosPreferidos?.joinToString(", ")}
+        """.trimIndent()
+
+        binding.buttonFechar.setOnClickListener {
+            finishAffinity()
         }
     }
 }
